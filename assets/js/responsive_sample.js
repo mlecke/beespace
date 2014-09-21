a3b6dd0dd07db8cd1c2c080329f5aab0d6e5b87c
Michael Lecke <mlecke@beespace.com>
2014-09-21 09:58:28 -0400

    css input fields
    css input fields, index placeholder, responsive sample js

---
+++ assets/js/responsive_sample.js
@@ -0,0 +1,29 @@
+// ┌────────────────────────────────────────────────────────────────────┐ \\
+// │ Beespace 0.1 - JavaScript Library                                  │ \\
+// ├────────────────────────────────────────────────────────────────────┤ \\
+// │ Copyright © 2014 Michael Lecke (http://beespace.com)               │ \\
+// └────────────────────────────────────────────────────────────────────┘ \\
+
+$(document).ready(function(){
+/* test for javascript
+
+
+
+/*
+/* 
+
+
+/* get dimensions, set largest square, get center x,y coordinates
+/* if combCellCenter is true, redraw with new center 
+/* else make combCellCenter object, draw center comb cell, fade in
+/*
+/* set combCellCenter prototype properties
+/* draw surrounding cells, fade in
+/* set as flexible comb with defined behaviors
+/* get buzzwords
+/* pull Twitter data using API
+/* parse into terms, rank
+/* draw clouds with z-index depth
+*/
+ 
+  });
